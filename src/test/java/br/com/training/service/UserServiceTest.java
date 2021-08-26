4ff9ce2d4ba9fcbabefd7264944a98335796eb9f
Rafael <rafa.s.francisco@gmail.com>
2021-08-26 13:30:11 -0300

    trabalhando com JUnit

---
+++ src/test/java/br/com/training/service/UserServiceTest.java
@@ -0,0 +1,52 @@
+package br.com.training.service;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.training.model.User;
+import br.com.training.repository.UserRepository;
+
+@SpringBootTest
+@TestInstance(Lifecycle.PER_CLASS)
+public class UserServiceTest {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@BeforeAll
+	public void criaUserValido() {
+		User user = new User("Rafael", "rafa.s.francisco@gmail.com", "33293800807", LocalDate.of(2021, 07, 16));
+		
+		assertNotNull(repository.save(user));
+	}
+	
+	@Test
+	public void pesquisaSeUserExiste() {
+		assertNotNull(repository.findByCpf("33293800807"));
+	}
+	
+	@Test
+	public void pesquisaSeUserNaoExiste() {
+		assertNull(repository.findByCpf("1"));
+	}
+	
+	@Test
+	public void atualizaUser() {
+		
+		User user = repository.findByCpf("33293800807");
+		user.setBirthDate(LocalDate.of(1984, 07, 16));
+		
+		assertNotNull(repository.save(user));
+		
+	}
+ 
+}
