566bc60cc5aa2a3320601d1bca28dc5940403f27
Rafael <rafa.s.francisco@gmail.com>
2021-10-10 15:06:47 -0300

    implementando teste no metodo save da classe UserService.

--- src/test/java/br/com/training/service/UserServiceTestMock.java
+++ src/test/java/br/com/training/service/UserServiceTestMock.java
@@ -2,12 +2,17 @@ package br.com.training.service;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import br.com.training.dto.UserForm;
 import br.com.training.exceptions.UserNotFoundException;
+import br.com.training.model.User;
 import br.com.training.repository.UserRepository;
 
 public class UserServiceTestMock {
@@ -20,11 +25,28 @@ public class UserServiceTestMock {
 	@BeforeEach
 	public void initMock() {
 		MockitoAnnotations.openMocks(this);
+		service = new UserService(repository);
 	}
 	
 	@Test
 	public void throwNotFoundException() {
-		service = new UserService(repository);
 		assertThrows(UserNotFoundException.class, () -> service.findByCpf("3"));
+	}
+	
+	@Test
+	public void saveUser() {
+		UserForm form = new UserForm();
+		form.setName("Fulano");
+		form.setEmail("fulano@gmail.com");
+		form.setCpf("123456789");
+		form.setBirthDate(LocalDate.of(1984, 7, 16));
+		
+		User user = new User(form.getName(), form.getEmail(), form.getCpf(), form.getBirthDate());
+
+		Mockito.when(repository.save(user)).thenReturn(user);
+		
+		service.save(form);
+		
+		Mockito.verify(repository).save(user);
 	}
 }
