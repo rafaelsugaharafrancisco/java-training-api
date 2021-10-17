54d1a3bc357b12c8cd4cd968c0fa69aa3ec0c09e
Rafael <rafa.s.francisco@gmail.com>
2021-10-17 20:06:39 -0300

    configurando CORS para testar o front-end desenvolvido com Angular

---
+++ src/main/java/br/com/training/security/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package br.com.training.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT");
+	}
+}
