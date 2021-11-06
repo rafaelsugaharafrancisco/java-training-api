12648c7ee51546d1b274b35bdf9ff43dc6186815
Rafael <rafa.s.francisco@gmail.com>
2021-11-05 22:03:14 -0300

    melhorando o swagger

--- src/main/java/br/com/training/config/SwaggerConfig.java
+++ src/main/java/br/com/training/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package br.com.training.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,8 +20,20 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
           .select()
-          .apis(RequestHandlerSelectors.any())
+          .apis(RequestHandlerSelectors.basePackage("br.com.training.controller"))
           .paths(PathSelectors.any())
-          .build();
+          .build()
+          .apiInfo(apiInfo());
+    }
+    
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Treino API REST")
+                .description("API REST de usuários construído por Guillaume e melhorado por Rafael")
+//                .version("1.0.0")
+//                .license("Apache License Version 2.0")
+//                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+//                .contact(new Contact("Rafael", "rafa.s.francisco@gmail.com"))
+                .build();
     }
 }
